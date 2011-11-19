8211f8e829454b321adb5f06724feaf35da6c27c
zumbojo <zumbojo@gmail.com>
2011-11-19 12:42:23 -0600

    Initial commit.

---
+++ UIMadBro-Demo/UIMadBro-Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIMadBro-Demo
+//
+//  Created by user on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIMadBro-Demo/UIMadBro-Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIMadBro-Demo
+//
+//  Created by user on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
