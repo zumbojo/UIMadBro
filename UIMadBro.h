181ac9831cb1da21dc0533492c2c919bb6ad6670
zumbojo <zumbojo@gmail.com>
2011-11-19 13:05:37 -0600

    UIMadBro.h/m created as UIView subclass.

---
+++ UIMadBro.h
@@ -0,0 +1,13 @@
+//
+//  UIMadBro.h
+//  UIMadBro-Demo
+//
+//  Created by user on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIMadBro : UIView
+
+@end
---
+++ UIMadBro.m
@@ -0,0 +1,31 @@
+//
+//  UIMadBro.m
+//  UIMadBro-Demo
+//
+//  Created by user on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIMadBro.h"
+
+@implementation UIMadBro
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
